b9d92f5bc3bcd0037e8796504f3e9908e4d25d19
viiskies <viiskis@gmail.com>
2018-02-03 23:19:38 +0200

    goToOtherPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TaisyklesPage } from '../../pages/taisykles/taisykles';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  goToOtherPage() {
+    //push another page onto the history stack
+    //causing the nav controller to animate the new page in
+    this.navCtrl.push(TaisyklesPage);
   }
-
 }
